e77c695adcb1bb357a502ca52a12d03162b596d0
respeakerwork <costelloconcepts@gmail.com>
2019-05-31 18:34:44 +0000

    work

--- pulse_snowboy_test.cc
+++ pulse_snowboy_test.cc
@@ -90,7 +90,7 @@ int main(int argc, char *argv[]) {
     unique_ptr<Snowboy1bDoaKwsNode> snowboy_kws;
     unique_ptr<ReSpeaker> respeaker;
     collector.reset(PulseCollectorNode::Create_48Kto16K(source, BLOCK_SIZE_MS));
-    vep_1beam.reset(VepAecBeamformingNode::Create(StringToMicType(mic_type), false, 6, enable_wav));
+    vep_1beam.reset(VepAecBeamformingNode::Create(StringToMicType(mic_type), true, 6, enable_wav));
     // vep_1beam.reset(VepAec1BeamNode::Create(LINEAR_6MIC_8BEAM, 6));
     if (kws == "alexa") {
         cout << "using alexa kws" << endl;
